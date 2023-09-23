f39b19f15d05f70caddf9b8679506c8fce55b4f1
Ильичев Роман <roman.ilichev.ricpeekt@mail.ru>
2023-09-23 15:00:17 +0300

    Создание функций для работы со структурами
    1) Создание функций, с помощью которых создаются новые трубопроводы и КС путём ввода данных через консоль.
    2) Функции, которые позволяют посмотреть информацию о трубопроводе и КС
    3) Функции для редактирования таких параметров, как количество работающих цехов на КС и признака трубопровода "В ремонте"

--- Pipeline transportation of gas and oil/Pipeline transportation of gas and oil.cpp
+++ Pipeline transportation of gas and oil/Pipeline transportation of gas and oil.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 struct Pipeline
 {
     string km_mark;
@@ -15,21 +16,135 @@ struct Pipeline
 struct CompressorStation
 {
     string title;
-    int all_number_workshop;
-    int active_number_workshop;
+    int all_workshop;
+    int active_workshop;
     float efficiency;
 };
 
 
+Pipeline CreatePipeline()
+{
+    Pipeline pl;
+
+    cout << "Kilometer mark: ";
+    cin >> pl.km_mark;
+
+    cout << "Length (km): ";
+    cin >> pl.length;
+
+    cout << "Diameter (mm): ";
+    cin >> pl.diameter;
+
+    char in_repair;
+
+    cout << "Is this pipeline in repair? (y/n): ";
+    cin >> in_repair;
+    pl.in_repair = (in_repair == 'y') ? true : false;
+    return pl;
+}
+
+
+CompressorStation CreateCS()
+{
+    CompressorStation cs;
+
+    cout << "Title of CS: ";
+    cin >> cs.title;
+
+    cout << "Number of all workshop: ";
+    cin >> cs.all_workshop;
+
+    cout << "Number of active workshop: ";
+    cin >> cs.active_workshop;
+
+    cout << "Efficiency: ";
+    cin >> cs.efficiency;
+    
+    return cs;
+}
+
+
+void ShowPipeline(const Pipeline& pl)
+{
+    cout << "\tInformation about pipeline " << "\"" << pl.km_mark << "\"" << endl;
+    cout << "Kilometer mark: " << pl.km_mark << endl;
+    cout << "Length: " << pl.length << " km" << endl;
+    cout << "Diameter: " << pl.diameter << " mm" << endl;
+    
+    string in_repair;
+    in_repair = (pl.in_repair) ? "This pipeline is in repair" : "This pipeline is in working condition";
+    cout << in_repair << endl;
+}
+
+
+void ShowCS(const CompressorStation& cs)
+{
+    cout << "\tInformation about CS " << "\"" << cs.title << "\"" << endl;
+    cout << "Title: " << cs.title << endl;
+    cout << "Number of all workshop: " << cs.all_workshop << endl;
+    cout << "Nubmer of active workshop: " << cs.active_workshop << endl;
+    cout << "Efficiency: " << cs.efficiency << endl;
+}
+
+
+void EditPipeline(Pipeline& pl)
+{
+    char in_repair;
+
+    if (pl.in_repair)
+    {
+        cout << "This pipeline is in repair" << endl;
+        cout << "Open this pipeline for using? (y/n): ";
+        cin >> in_repair;
+        pl.in_repair = (in_repair == 'y') ? false : true;
+    }
+    else
+    {
+        cout << "This pipeline is in working condition" << endl;
+        cout << "Close this pipeline for reparing? (y/n): ";
+        cin >> in_repair;
+        pl.in_repair = (in_repair == 'y') ? true : false;
+    }
+}
+
+
+void EditCS(CompressorStation& cs)
+{
+    cout << "Currently number of active workshop is " <<
+        cs.active_workshop << " out of " <<
+        cs.all_workshop << endl;
+
+    cout << "Enter new number of active workshop: ";
+    cin >> cs.active_workshop;
+}
+
+
 int main()
 {
-    
+
     Pipeline pipe;
     CompressorStation station;
-    /*pipe.diameter = 10;
-    station.title = "Romashka";
-    cout << pipe.diameter << endl;
-    cout << station.active_number_workshop << endl;*/
+    pipe = CreatePipeline();
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    EditPipeline(pipe);
+    ShowPipeline(pipe);
+    station = CreateCS();
+    ShowCS(station);
+    EditCS(station);
+    ShowCS(station);
+    EditCS(station);
+    ShowCS(station);
+
 
 
 }
