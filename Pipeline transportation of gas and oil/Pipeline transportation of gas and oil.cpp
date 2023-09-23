186f4507685a3859e18531f98df33bd93fa1c0ad
Ильичев Роман <roman.ilichev.ricpeekt@mail.ru>
2023-09-23 12:23:26 +0300

    Создание пользовательского типа данных
    Создание структур, описывающих свойства трубопровода и компрессорной станции.

---
+++ Pipeline transportation of gas and oil/Pipeline transportation of gas and oil.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pipeline
+{
+    string km_mark;
+    int length;
+    int diameter;
+    bool in_repair;
+};
+
+
+struct CompressorStation
+{
+    string title;
+    int all_number_workshop;
+    int active_number_workshop;
+    float efficiency;
+};
+
+
+int main()
+{
+    
+    Pipeline pipe;
+    CompressorStation station;
+    /*pipe.diameter = 10;
+    station.title = "Romashka";
+    cout << pipe.diameter << endl;
+    cout << station.active_number_workshop << endl;*/
+
+
+}
+
+
